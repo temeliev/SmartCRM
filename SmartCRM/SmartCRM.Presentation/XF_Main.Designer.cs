096d25e0d4ba4d26d0eeebdf0cbfff34010cd74d
Denislav Temeliev <dtemeliev@yahoo.com>
2015-10-09 15:51:21 +0300

    added main form

--- SmartCRM/SmartCRM.Presentation/XF_Main.Designer.cs
+++ SmartCRM/SmartCRM.Presentation/XF_Main.Designer.cs
@@ -28,19 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlMain = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.btnCreateEditFirm = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.btnCustomers = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnEmployees = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem4 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.btnFirmInfo = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnSales = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
             this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btnSales);
+            this.layoutControl1.Controls.Add(this.btnFirmInfo);
+            this.layoutControl1.Controls.Add(this.btnEmployees);
+            this.layoutControl1.Controls.Add(this.btnCustomers);
+            this.layoutControl1.Controls.Add(this.btnCreateEditFirm);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(516, 40, 377, 441);
+            this.layoutControl1.Root = this.layoutControlMain;
+            this.layoutControl1.Size = new System.Drawing.Size(836, 464);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlMain
+            // 
+            this.layoutControlMain.CustomizationFormText = "Root";
+            this.layoutControlMain.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlMain.GroupBordersVisible = false;
+            this.layoutControlMain.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.emptySpaceItem1,
+            this.layoutControlItem2,
+            this.emptySpaceItem4,
+            this.emptySpaceItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5});
+            this.layoutControlMain.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlMain.Name = "Root";
+            this.layoutControlMain.Padding = new DevExpress.XtraLayout.Utils.Padding(3, 3, 3, 3);
+            this.layoutControlMain.Size = new System.Drawing.Size(836, 464);
+            this.layoutControlMain.Text = "Root";
+            this.layoutControlMain.TextVisible = false;
+            // 
+            // btnCreateEditFirm
+            // 
+            this.btnCreateEditFirm.Location = new System.Drawing.Point(5, 5);
+            this.btnCreateEditFirm.Name = "btnCreateEditFirm";
+            this.btnCreateEditFirm.Size = new System.Drawing.Size(142, 22);
+            this.btnCreateEditFirm.StyleController = this.layoutControl1;
+            this.btnCreateEditFirm.TabIndex = 1;
+            this.btnCreateEditFirm.Text = "Create";
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.btnCreateEditFirm;
+            this.layoutControlItem1.CustomizationFormText = "layoutControlCreateEdit";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(146, 26);
+            this.layoutControlItem1.Text = "layoutControlCreateEdit";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextToControlDistance = 0;
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.CustomizationFormText = "emptySpaceItem1";
+            this.emptySpaceItem1.Location = new System.Drawing.Point(146, 0);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(684, 26);
+            this.emptySpaceItem1.Text = "emptySpaceItem1";
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // emptySpaceItem2
+            // 
+            this.emptySpaceItem2.AllowHotTrack = false;
+            this.emptySpaceItem2.CustomizationFormText = "emptySpaceItem2";
+            this.emptySpaceItem2.Location = new System.Drawing.Point(0, 26);
+            this.emptySpaceItem2.Name = "emptySpaceItem2";
+            this.emptySpaceItem2.Size = new System.Drawing.Size(830, 20);
+            this.emptySpaceItem2.Text = "emptySpaceItem2";
+            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Location = new System.Drawing.Point(5, 51);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(411, 135);
+            this.btnCustomers.StyleController = this.layoutControl1;
+            this.btnCustomers.TabIndex = 4;
+            this.btnCustomers.Text = "Customers";
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnCustomers;
+            this.layoutControlItem2.CustomizationFormText = "layoutControlCustomers";
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 46);
+            this.layoutControlItem2.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(415, 139);
+            this.layoutControlItem2.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem2.Text = "layoutControlCustomers";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextToControlDistance = 0;
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Location = new System.Drawing.Point(420, 51);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(411, 135);
+            this.btnEmployees.StyleController = this.layoutControl1;
+            this.btnEmployees.TabIndex = 5;
+            this.btnEmployees.Text = "Employees";
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnEmployees;
+            this.layoutControlItem3.CustomizationFormText = "layoutControlEmployees";
+            this.layoutControlItem3.Location = new System.Drawing.Point(415, 46);
+            this.layoutControlItem3.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(415, 139);
+            this.layoutControlItem3.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem3.Text = "layoutControlEmployees";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextToControlDistance = 0;
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // emptySpaceItem4
+            // 
+            this.emptySpaceItem4.AllowHotTrack = false;
+            this.emptySpaceItem4.CustomizationFormText = "emptySpaceItem4";
+            this.emptySpaceItem4.Location = new System.Drawing.Point(0, 324);
+            this.emptySpaceItem4.Name = "emptySpaceItem4";
+            this.emptySpaceItem4.Size = new System.Drawing.Size(830, 134);
+            this.emptySpaceItem4.Text = "emptySpaceItem4";
+            this.emptySpaceItem4.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // btnFirmInfo
+            // 
+            this.btnFirmInfo.Location = new System.Drawing.Point(5, 190);
+            this.btnFirmInfo.Name = "btnFirmInfo";
+            this.btnFirmInfo.Size = new System.Drawing.Size(411, 135);
+            this.btnFirmInfo.StyleController = this.layoutControl1;
+            this.btnFirmInfo.TabIndex = 6;
+            this.btnFirmInfo.Text = "Personal Data";
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btnFirmInfo;
+            this.layoutControlItem4.CustomizationFormText = "layoutControlPersonalData";
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 185);
+            this.layoutControlItem4.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(415, 139);
+            this.layoutControlItem4.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem4.Text = "layoutControlPersonalData";
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextToControlDistance = 0;
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // btnSales
+            // 
+            this.btnSales.Location = new System.Drawing.Point(420, 190);
+            this.btnSales.Name = "btnSales";
+            this.btnSales.Size = new System.Drawing.Size(411, 135);
+            this.btnSales.StyleController = this.layoutControl1;
+            this.btnSales.TabIndex = 7;
+            this.btnSales.Text = "Sales";
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.btnSales;
+            this.layoutControlItem5.CustomizationFormText = "layoutControlSales";
+            this.layoutControlItem5.Location = new System.Drawing.Point(415, 185);
+            this.layoutControlItem5.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(415, 139);
+            this.layoutControlItem5.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem5.Text = "layoutControlSales";
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextToControlDistance = 0;
+            this.layoutControlItem5.TextVisible = false;
             // 
             // XF_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 474);
+            this.ClientSize = new System.Drawing.Size(836, 464);
+            this.Controls.Add(this.layoutControl1);
             this.Name = "XF_Main";
-            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlMain;
+        private DevExpress.XtraEditors.SimpleButton btnCreateEditFirm;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+        private DevExpress.XtraEditors.SimpleButton btnSales;
+        private DevExpress.XtraEditors.SimpleButton btnFirmInfo;
+        private DevExpress.XtraEditors.SimpleButton btnEmployees;
+        private DevExpress.XtraEditors.SimpleButton btnCustomers;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem4;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
     }
 }
--- SmartCRM/SmartCRM.Presentation/XF_Main.cs
+++ SmartCRM/SmartCRM.Presentation/XF_Main.cs
@@ -7,6 +7,42 @@
         public XF_Main()
         {
             this.InitializeComponent();
+            this.Load += this.XF_Main_Load;
+            this.btnCreateEditFirm.Click += this.btnCreateEditFirm_Click;
+            this.btnCustomers.Click += this.btnCustomers_Click;
+            this.btnEmployees.Click += this.btnEmployees_Click;
+            this.btnFirmInfo.Click += this.btnFirmInfo_Click;
+            this.btnSales.Click += this.btnSales_Click;
         }
+
+        void XF_Main_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        void btnSales_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void btnFirmInfo_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void btnEmployees_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void btnCustomers_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void btnCreateEditFirm_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }       
     }
 }
