bdf004f5931b914fb694802c037d416851ef2a4b
Denislav Temeliev <dtemeliev@yahoo.com>
2015-10-09 17:16:49 +0300

    Added login form

---
+++ SmartCRM/SmartCRM.Presentation/XF_Login.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SmartCRM.Presentation
+{
+    partial class XF_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup3 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnEnter = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlEnter = new DevExpress.XtraLayout.LayoutControlItem();
+            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlUsername = new DevExpress.XtraLayout.LayoutControlItem();
+            this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlPassword = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlEnter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlUsername)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "Root";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(5, 5, 5, 5);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(357, 151);
+            this.layoutControlGroup1.Text = "Root";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlGroup2
+            // 
+            this.layoutControlGroup2.CustomizationFormText = "Root";
+            this.layoutControlGroup2.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup2.GroupBordersVisible = false;
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup2.Name = "Root";
+            this.layoutControlGroup2.Padding = new DevExpress.XtraLayout.Utils.Padding(5, 5, 5, 5);
+            this.layoutControlGroup2.Size = new System.Drawing.Size(357, 151);
+            this.layoutControlGroup2.Text = "Root";
+            this.layoutControlGroup2.TextVisible = false;
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.txtPassword);
+            this.layoutControl1.Controls.Add(this.txtUsername);
+            this.layoutControl1.Controls.Add(this.btnEnter);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(471, 63, 250, 350);
+            this.layoutControl1.Root = this.layoutControlGroup3;
+            this.layoutControl1.Size = new System.Drawing.Size(354, 142);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup3
+            // 
+            this.layoutControlGroup3.CustomizationFormText = "Root";
+            this.layoutControlGroup3.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup3.GroupBordersVisible = false;
+            this.layoutControlGroup3.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlEnter,
+            this.layoutControlUsername,
+            this.layoutControlPassword,
+            this.emptySpaceItem1});
+            this.layoutControlGroup3.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup3.Name = "Root";
+            this.layoutControlGroup3.Padding = new DevExpress.XtraLayout.Utils.Padding(3, 3, 3, 3);
+            this.layoutControlGroup3.Size = new System.Drawing.Size(354, 142);
+            this.layoutControlGroup3.Text = "Root";
+            this.layoutControlGroup3.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.CustomizationFormText = "Потребител:";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(347, 34);
+            this.layoutControlItem1.Text = "Потребител:";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(65, 13);
+            this.layoutControlItem1.TextToControlDistance = 5;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEnter.Appearance.Options.UseFont = true;
+            this.btnEnter.Location = new System.Drawing.Point(5, 84);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(344, 53);
+            this.btnEnter.StyleController = this.layoutControl1;
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "LOGIN";
+            // 
+            // layoutControlEnter
+            // 
+            this.layoutControlEnter.Control = this.btnEnter;
+            this.layoutControlEnter.CustomizationFormText = "layoutControlEnter";
+            this.layoutControlEnter.ImageToTextDistance = 20;
+            this.layoutControlEnter.Location = new System.Drawing.Point(0, 79);
+            this.layoutControlEnter.MinSize = new System.Drawing.Size(82, 26);
+            this.layoutControlEnter.Name = "layoutControlEnter";
+            this.layoutControlEnter.Size = new System.Drawing.Size(348, 57);
+            this.layoutControlEnter.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlEnter.Text = "layoutControlEnter";
+            this.layoutControlEnter.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlEnter.TextToControlDistance = 0;
+            this.layoutControlEnter.TextVisible = false;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(61, 5);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtUsername.Properties.Appearance.Options.UseFont = true;
+            this.txtUsername.Size = new System.Drawing.Size(288, 26);
+            this.txtUsername.StyleController = this.layoutControl1;
+            this.txtUsername.TabIndex = 5;
+            // 
+            // layoutControlUsername
+            // 
+            this.layoutControlUsername.Control = this.txtUsername;
+            this.layoutControlUsername.CustomizationFormText = "Username:";
+            this.layoutControlUsername.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlUsername.MaxSize = new System.Drawing.Size(348, 30);
+            this.layoutControlUsername.MinSize = new System.Drawing.Size(348, 30);
+            this.layoutControlUsername.Name = "layoutControlUsername";
+            this.layoutControlUsername.Size = new System.Drawing.Size(348, 30);
+            this.layoutControlUsername.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlUsername.Text = "Username:";
+            this.layoutControlUsername.TextSize = new System.Drawing.Size(52, 13);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(61, 35);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPassword.Properties.Appearance.Options.UseFont = true;
+            this.txtPassword.Size = new System.Drawing.Size(288, 26);
+            this.txtPassword.StyleController = this.layoutControl1;
+            this.txtPassword.TabIndex = 6;
+            // 
+            // layoutControlPassword
+            // 
+            this.layoutControlPassword.Control = this.txtPassword;
+            this.layoutControlPassword.CustomizationFormText = "Password:";
+            this.layoutControlPassword.Location = new System.Drawing.Point(0, 30);
+            this.layoutControlPassword.MaxSize = new System.Drawing.Size(348, 30);
+            this.layoutControlPassword.MinSize = new System.Drawing.Size(348, 30);
+            this.layoutControlPassword.Name = "layoutControlPassword";
+            this.layoutControlPassword.Size = new System.Drawing.Size(348, 30);
+            this.layoutControlPassword.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlPassword.Text = "Password:";
+            this.layoutControlPassword.TextSize = new System.Drawing.Size(52, 13);
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.CustomizationFormText = "emptySpaceItem1";
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 60);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(348, 19);
+            this.emptySpaceItem1.Text = "emptySpaceItem1";
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // XF_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 142);
+            this.Controls.Add(this.layoutControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "XF_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlEnter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlUsername)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraEditors.TextEdit txtPassword;
+        private DevExpress.XtraEditors.TextEdit txtUsername;
+        private DevExpress.XtraEditors.SimpleButton btnEnter;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlEnter;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlUsername;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlPassword;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+    }
+}
---
+++ SmartCRM/SmartCRM.Presentation/XF_Login.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartCRM.Presentation
+{
+    using DevExpress.XtraEditors;
+
+    public partial class XF_Login : XtraForm
+    {
+        public XF_Login()
+        {
+            this.InitializeComponent();
+            this.btnEnter.Click += this.btnEnter_Click;
+        }
+
+        void btnEnter_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
